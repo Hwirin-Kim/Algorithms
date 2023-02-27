cf39e1d8dedba710f2d844544fa438b9fda9345b
[Hwirin] <[kimhuirin@gmail.com]>
2023-02-27 11:04:59 +0900

    20230227 알고리즘풀이

---
+++ hello.js
@@ -0,0 +1,11 @@
+// dev/stdin
+
+var fs = require("fs");
+var input = fs.readFileSync("input.txt").toString();
+input = input.split(" ");
+console.log(input[0] + input[1]);
+console.log(`|\\_/|`);
+console.log(`|q p|   /}`);
+console.log(`( 0 )"""\\`);
+console.log(`|"^"\`    |`);
+console.log(`||_/=\\\\__|`);
