df18e27e20a76ed0ea0694a44925218c1f3095fb
Hwirin-Kim <[kimhuirin@gmail.com]>
2023-03-05 18:16:10 +0900

    20230305 알고리즘 문제풀이

--- test.js
+++ test.js
@@ -1,17 +1,69 @@
-let array = [1, 5, 2, 6, 3, 7, 4];
-let commands = [
-  [2, 5, 3],
-  [4, 4, 1],
-  [1, 7, 3],
+let id_list = ["muzi", "frodo", "apeach", "neo"];
+
+let report = [
+  "muzi frodo",
+  "apeach frodo",
+  "frodo neo",
+  "muzi neo",
+  "apeach muzi",
 ];
 
-function solution(array, commands) {
-  let answer = commands.map((command) => {
-    return array.slice(command[0] - 1, command[1]).sort(function (a, b) {
-      return a - b;
-    })[command[2] - 1];
+let k = 2;
+
+//id별 신고한 목록
+function getMyReportInfo(id_list, report) {
+  let myReportInfo = id_list.map((id) => {
+    let myReport_list = [];
+    for (let i = 0; i < report.length; i++) {
+      //아이디가 같으면서, 중복된것이 없도록 if를 걸었음
+      if (
+        report[i].split(" ")[0] === id &&
+        !myReport_list.includes(report[i].split(" ")[1])
+      ) {
+        myReport_list.push(report[i].split(" ")[1]);
+      }
+    }
+    return { id, myReport: myReport_list };
   });
-  return answer;
+  return myReportInfo;
+}
+function getIsReportedInfo(myReportInfo, id_list, k) {
+  let isReportedInfo = id_list.map((id) => {
+    // isReported 각 id가 신고당한 횟수
+    let isReported;
+    let isReportedCnt = 0;
+    for (let i = 0; i < myReportInfo.length; i++) {
+      for (let j = 0; j < myReportInfo[i].myReport.length; j++) {
+        if (myReportInfo[i].myReport[j] === id) {
+          isReportedCnt = isReportedCnt + 1;
+        }
+      }
+    }
+    if (isReportedCnt >= k) {
+      isReported = true;
+    } else {
+      isReported = false;
+    }
+    return { id, isReported };
+  });
+  return isReportedInfo;
 }
 
-console.log(solution(array, commands));
+function getCnt(myReportInfo, isReportedInfo) {
+  let cnt = myReportInfo.map((info) => {
+    let cnt = 0;
+    for (let i = 0; i < isReportedInfo.length; i++) {
+      if (
+        isReportedInfo[i].isReported === true &&
+        info.myReport.includes(isReportedInfo[i].id)
+      ) {
+        cnt = cnt + 1;
+      }
+    }
+    return cnt;
+  });
+  return cnt;
+}
+const myReportInfo = getMyReportInfo(id_list, report);
+const isReportedInfo = getIsReportedInfo(myReportInfo, id_list, k);
+getCnt(myReportInfo, isReportedInfo);
